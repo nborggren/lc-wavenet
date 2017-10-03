b0c499f556772af19dccb38a44586b462609af84
thakkarv <thakkarv@bu.edu>
2017-10-03 09:23:14 -0400

    Dimentions mismatch between queue placeholder and queue expected

--- wavenet/lc_audio_reader.py
+++ wavenet/lc_audio_reader.py
@@ -163,8 +163,8 @@ class LCAudioReader():
 
 		if self.lc_enabled:	
 			# LC samples are embedding vectors with the shape of 1 X LC_channels
-			self.lc_placeholder = tf.placeholder(dtype = tf.float32, shape = (1, self.lc_channels))
-			self.q_lc = tf.PaddingFIFOQueue(capacity = q_size, dtypes = [tf.float32], shapes = [(None,  self.lc_channels)])
+			self.lc_placeholder = tf.placeholder(dtype = tf.float32, shape = (self.lc_channels))
+			self.q_lc = tf.PaddingFIFOQueue(capacity = q_size, dtypes = [tf.float32], shapes = [(self.lc_channels)])
 			self.enq_lc = self.q_lc.enqueue([self.lc_placeholder])
 
 		# now load in the files and see if they exist
